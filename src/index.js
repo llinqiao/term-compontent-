189e0b3794c1e9b57e3e6475adb71d490adc82c9
llq6 <984673705@qq.com>
2018-06-27 21:42:42 +0800

    hyperapp listt

--- src/index.js
+++ src/index.js
@@ -32,20 +32,54 @@ const List = ({ lists, actions }) => (
   </div>
 );
 
-
+const Term = ({ terms, actions }) => (
+  <div 
+    oncreate={() => {
+      fetch("/api/topics/latest.json")
+        .then(res => {
+          return res.json();
+        })
+        .then(re => {
+          actions.setTerm(re);
+        });
+    }}
+    className="container-border center"
+  >
+    {terms.map(item => {
+      var information = item.url;
+      var image = item.member.avatar_normal;
+      var id = item.member.id
+      return<div className="content">
+              <div className="image">
+                <img src={image}/>
+              </div>
+              <div className="item">
+                <div className="text">
+                  <a href={information}>{item.title}</a>
+                </div>
+                <a href={information}>{id}</a>
+              </div>
+            </div>;
+    })}
+  </div>
+);
 
 const state = {
-  lists: []
+  lists: [],
+  terms: []
 };
 
 const actions = {
-  setList: value => state => ({ lists: value })
+  setList: value => state => ({ lists: value }),
+  setTerm: value => state => ({ terms: value })
 };
 
 const view = (state, actions) => (
   <div className="container">
     <List lists={state.lists} actions={actions} />
+    <Term terms={state.terms} actions={actions} />
   </div>
 );
 
 app(state, actions, view, document.body);
+
